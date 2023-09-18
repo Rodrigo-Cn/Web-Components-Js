ce881e0be5560b392cd0c866933120679229b3ad
Rodrigo-Cn <125518378+Rodrigo-Cn@users.noreply.github.com>
2023-09-18 11:42:33 -0300

    third commit

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ class CardNews extends HTMLElement {
     constructor(){
         super();
         const shadow = this.attachShadow({mode: "open"})
-        shadow.innerHTML = "<h1>Hello World</h1>"
+        shadow.innerHTML = "<h1>Hello World</h1><button>Rodrigo</button>"
     }
 }
 
 customElements.define('card-news',CardNews)
+
+class cardOne extends HTMLElement{
+    constructor(){
+        super();
+        const shadow2 = this.attachShadow({mode:"open"})
+        shadow2.innerHTML = "<h2>teste2</h2>"
+    }
+}
+
+customElements.define("card-news2",cardOne)
