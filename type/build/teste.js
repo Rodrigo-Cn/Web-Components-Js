fdd0ae6e045cb96d5721132d11d0a2df761a5743
Rodrigo-Cn <125518378+Rodrigo-Cn@users.noreply.github.com>
2023-09-22 18:54:54 -0300

    tenth commit

--- type/build/teste.js
+++ type/build/teste.js
@@ -7,3 +7,28 @@ tipoObjeto({
     nome: "Rodrigo",
     sobrenome: "Costa"
 });
+var idade;
+var persona = {
+    nome: "Rodrigo",
+    sobrenome: "Costa"
+};
+var conta = ["Rodrigo", 100.0, 136532552];
+var conta2 = ["Rodrigo", 13];
+function pessoa(id) {
+    let nome = "Exemplo";
+    return nome;
+}
+const bot = {
+    id: 1,
+    nome: "Rodrigo"
+};
+const bott = {
+    id: 1,
+    nome: "Rodrigo"
+};
+class Batman {
+    constructor(id, nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+}
--- type/src/teste.ts
+++ type/src/teste.ts
@@ -12,3 +12,54 @@ tipoObjeto({
     nome:"Rodrigo",
     sobrenome:"Costa"
 })
+
+var idade:number;
+
+var persona:object = {
+    nome:"Rodrigo",
+    sobrenome:"Costa"
+}
+
+type persona2 = {
+    nome:string;
+    idade:number;
+}
+
+var conta:[string,number,number] = ["Rodrigo",100.0,136532552];
+
+var conta2: (string | number)[] = ["Rodrigo", 13];
+
+function pessoa(id: number): string | number {
+    let nome: string = "Exemplo";
+    return nome;
+}
+
+type bot1 = {
+    readonly id:number;
+    nome:string;
+}
+
+interface bot2 {
+    readonly id:number,
+    nome:string
+}
+
+const bot:bot1 = {
+    id:1,
+    nome:"Rodrigo"
+}
+
+const bott:bot2 = {
+    id:1,
+    nome:"Rodrigo"
+}
+
+class Batman implements bot1 {
+    readonly id: number;
+    nome: string;
+    
+    constructor(id: number, nome: string) {
+        this.id = id;
+        this.nome = nome;
+    }
+}
